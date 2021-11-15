dd5d9e6316d9770473c6a429cbd6b141327369e1
Mathieu <mathieu.linder.dev@gmail.com>
2021-11-15 07:41:04 +0100

    completed project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'antd/dist/antd.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from './app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>,
   document.getElementById('root')
 );
